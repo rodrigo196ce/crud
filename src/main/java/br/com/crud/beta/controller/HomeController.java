6340b058a966b053fed33c3e97f577f7f620c77c
Rodrigo Souza <rodrigo@MacBook-Air-de-Rodrigo.local>
2022-07-21 14:23:34 -0300

    Primeiro push do projeto

---
+++ src/main/java/br/com/crud/beta/controller/HomeController.java
@@ -0,0 +1,16 @@
+package br.com.crud.beta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class HomeController {
+
+    @RequestMapping()
+    public String home(){
+        return "home/home.html";
+    }
+
+
+}
