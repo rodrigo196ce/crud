a97dc1e71bcd428a90aca5c98e3ce97a30ca7593
Rodrigo Souza <rodrigo@MacBook-Air-de-Rodrigo.local>
2022-07-25 11:09:58 -0300

    Adicionando o modelo de Entidade 'User'

--- src/main/java/br/com/crud/beta/model/User.java
+++ src/main/java/br/com/crud/beta/model/User.java
@@ -1,5 +1,130 @@
 package br.com.crud.beta.model;
 
+import br.com.crud.beta.enums.Cidade;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="users")
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+    private LocalDate dataNascimento;
+    private String email;
+    private String telefone;
+    private String rua;
+    private Integer numero;
+    private String complemento;
+    private String bairro;
+    @Enumerated(EnumType.STRING)
+    private Cidade cidade;
+    private String cep;
+
+    public User(){}
+    public User(Integer id, String nome, LocalDate dataNascimento, String email, String telefone, String rua, Integer numero,
+                String complemento, String bairro, Cidade cidade, String cep) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.telefone = telefone;
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 }
